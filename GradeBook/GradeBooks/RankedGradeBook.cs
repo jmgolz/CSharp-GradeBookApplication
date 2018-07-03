639f70e27fa6c881d7396184b6b00a7ae3312620
James Golz <james.golz@assistrx.com>
2018-07-03 15:20:37 -0400

    Initial add of grades ranking

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,24 @@ namespace GradeBook.GradeBooks
 			{
 				 Type = Enums.GradeBookType.Ranked;
 			}
+
+			public override char GetLetterGrade(double averageGrade)
+			{
+				 if(Students.Count < 5){
+						throw new InvalidOperationException("Ranked grading requires a minimum of 5 students to work.");
+				 }
+
+				 if(averageGrade <= 0.20){
+						return 'A';
+				 } else if (averageGrade > 0.20 && averageGrade <= 0.40) {
+						return 'B';
+				 } else if (averageGrade > 0.40 && averageGrade <= 0.60) {
+						return 'C';
+				 } else if(averageGrade > 0.60 && averageGrade <= 0.80) {
+						return 'D';
+				 } else {
+						return 'F';
+				 }
+			}
     }
 }
